bbb494991ecd3f63c52c864edb8636c73487201f
Jan Ludvig Vinningland <janludvig@gmail.com>
2025-09-17 10:56:48 +0200

    Moved code into core.py from Files.py

--- src/ECLlib/constants.py
+++ src/ECLlib/constants.py
@@ -1,4 +1,8 @@
 from ECLlib import unfmt_block
 
+DEBUG = False
+ENDIAN = '>'  # Big-endian
+ECL2IX_LOG = 'ecl2ix.log'
+
 # Empty block that terminates a SEQNUM - ENDSOL section in UNRST-files
 ENDSOL = unfmt_block.from_data('ENDSOL', [], 'mess')
