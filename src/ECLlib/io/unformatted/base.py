4a9888ad5df40dbaf47e799f34b85ff067bf9305
Jan Ludvig Vinningland <janludvig@gmail.com>
2025-09-23 15:18:01 +0200

    Refactor numpy imports for improved readability in base.py

--- src/ECLlib/io/unformatted/base.py
+++ src/ECLlib/io/unformatted/base.py
@@ -5,20 +5,12 @@ from mmap import mmap, ACCESS_READ
 from operator import sub as subtract
 from struct import pack, unpack, error as struct_error
 
-from numpy import (
-    array as nparray,
-    asarray,
-    char as npchar,
-    cumsum,
-    dtype as npdtype,
-    frombuffer,
-    ndarray,
-    split as npsplit,
-)
+from numpy import (array as nparray, asarray, char as npchar, cumsum, dtype as npdtype,
+    frombuffer, ndarray, split as npsplit)
 
 from ...core import File, DTYPE
 from ...config import DEBUG, ENDIAN
-from ...utils import (batched, batched_when, ensure_bytestring, expand_pattern, flatten, flatten_all, 
+from ...utils import (batched, batched_when, ensure_bytestring, expand_pattern, flatten, flatten_all,
     index_limits, match_in_wildlist, nth, pad, pairwise, slice_range, string_split, take)
 
 __all__ = ["unfmt_header", "unfmt_block", "unfmt_file", "ENDSOL"]
