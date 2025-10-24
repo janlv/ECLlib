df79eea09a6aaaae73372f7baa4c058b8fcec27c
Jan Ludvig Vinningland <janludvig@gmail.com>
2025-10-24 09:31:59 +0200

    Refactor import statements in output module for clarity and consistency

--- src/ECLlib/io/output/__init__.py
+++ src/ECLlib/io/output/__init__.py
@@ -1,13 +1,8 @@
 """Output-related helpers for ECLlib."""
 
 from .egrid_file import EGRID_file
-from .unformatted_files import (
-    INIT_file,
-    RFT_file,
-    SMSPEC_file,
-    UNRST_file,
-    UNSMRY_file,
-)
+from .unformatted_files import (INIT_file, RFT_file, SMSPEC_file, UNRST_file, UNSMRY_file,
+                                RSSPEC_file)
 from .textual_files import MSG_file, PRTX_file, PRT_file, text_file
 from .formatted_files import FUNRST_file, RSM_block, RSM_file, fmt_block, fmt_file
 
@@ -17,6 +12,7 @@ __all__ = [
     "UNRST_file",
     "RFT_file",
     "UNSMRY_file",
+    "RSSPEC_file",
     "SMSPEC_file",
     "text_file",
     "MSG_file",
